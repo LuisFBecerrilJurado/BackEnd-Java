37988a1aaeb6197da8bf027cf84e0a263b163198
Luis Francisco Becerril Jurado <99098643+LuisFBecerrilJurado@users.noreply.github.com>
2022-04-21 00:18:24 -0500

    Add practica HarryPotter

---
+++ practicaHarry/src/HarryPoter/Snape.java
@@ -0,0 +1,18 @@
+package HarryPoter;
+
+public class Snape {
+    private String casa, genero, nombre, boggart, patronus;
+    public Snape(String casa, String genero, String nombre, String boggart, String patronus){
+        this.casa = casa;
+        this.genero = genero;
+        this.nombre = nombre;
+        this.boggart = boggart;
+        this.patronus = patronus;
+    }
+
+    public String getCasa(){return casa;}
+    public String getGenero() {return genero;}
+    public String getNombre() {return nombre;}
+    public String getBoggart() {return boggart;}
+    public String getPatronus() {return patronus;}
+}
